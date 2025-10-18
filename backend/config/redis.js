86f19965f0feecda8ef6923fa887a74005fc05b9
Khizar-cyber-dev <hafizkhizarkhizar07@gmail.com>
2025-10-18 19:41:21 +0500

    fix the package issue.

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,4 +1,5 @@
 import { Redis } from '@upstash/redis'
+
 const redis = new Redis({
   url: 'https://fun-koi-17228.upstash.io',
   token: 'AUNMAAIncDI0N2Q5ZTQ1ZWJkMDQ0N2IwODYwMWZlYjdkMmUyYzQyNnAyMTcyMjg',
